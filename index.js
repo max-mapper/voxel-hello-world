e3c3271eeb789c133200698069db9e82d203eedb
Max Ogden <max@maxogden.com>
2013-03-29 14:40:43 -0700

    fix demo

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var texturePath = require('painterly-textures')(__dirname)
 var voxel = require('voxel')
 var extend = require('extend')
 
-module.exports = function(opts) {
+module.exports = function(opts, setup) {
+  setup = setup || defaultSetup
   var defaults = {
     generate: voxel.generator['Valley'],
     chunkDistance: 2,
@@ -24,6 +25,7 @@ module.exports = function(opts) {
 
   // setup the game and add some trees
   var game = createGame(opts)
+  var createPlayer = player(game)
 
   window.game = game // for debugging
   var container = opts.container || document.body
@@ -32,14 +34,20 @@ module.exports = function(opts) {
 
   // create the player from a minecraft skin file and tell the
   // game to use it as the main player
-  var createPlayer = player(game)
-  var substack = createPlayer(opts.playerSkin || 'player.png')
-  substack.possess()
-  substack.yaw.position.set(2, 14, 4)
+  var avatar = createPlayer(opts.playerSkin || 'player.png')
+  avatar.possess()
+  avatar.yaw.position.set(2, 14, 4)
 
+  setup(game, avatar)
+  
+  return game
+
+}
+
+function defaultSetup(game, avatar) {
   // highlight blocks when you look at them, hold <Ctrl> for block placement
   var blockPosPlace, blockPosErase
-  var hl = game.highlighter = highlight(game, opts.highlightOpts || { color: 0xff0000 })
+  var hl = game.highlighter = highlight(game, { color: 0xff0000 })
   hl.on('highlight', function (voxelPos) { blockPosErase = voxelPos })
   hl.on('remove', function (voxelPos) { blockPosErase = null })
   hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos })
@@ -64,5 +72,4 @@ module.exports = function(opts) {
     }
   })
 
-  return game
 }
